a9126216a4862e40a5cecc9d5776f6c57463d70f
MOHD SHUAIB <66258933+01shuaib01@users.noreply.github.com>
2022-01-18 10:47:19 +0530

    Create Array1.java

---
+++ Array/Array1.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        // System.out.println("shuaib");
+
+        int[] arr = new int[5];
+
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+
+        int size = 3;
+        for (int i = 0; i < 3; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
